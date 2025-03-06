05ff6373dba43b7cc49aad418a8643343bf2fa2a
Rakib Hossain <35700333+rakiblegends@users.noreply.github.com>
2025-03-06 15:17:22 +0600

    Dependency Injection via Constructor.

---
+++ DependenceInjections/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Concept
+{
+    public interface IVehicleInterface
+    {
+        void Start();
+    }
+    public class Bike : IVehicleInterface
+    {
+        public void Start()
+        {
+            Console.WriteLine("Bike Started");
+        }
+    }
+
+    public class Bus : IVehicleInterface
+    {
+        public void Start()
+        {
+            Console.WriteLine("Bus started");
+        }
+    }
+
+    public class Driver
+    {
+        private readonly IVehicleInterface _vehicleInterface;
+        public Driver(IVehicleInterface vehicleInterface)
+        {
+            _vehicleInterface = vehicleInterface;
+        }
+        public void Start()
+        {
+            _vehicleInterface.Start();
+        }
+    }
+}
---
+++ DependenceInjections/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP_Concept
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Bus bus = new Bus();
+            Bike bike = new Bike();
+            Driver driver = new Driver(bike);
+            //driver.Start();
+            driver.Start();
+        }
+    }
+}
+
