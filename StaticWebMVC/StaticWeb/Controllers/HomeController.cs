6157041d13e106702a1a248d52a6a69aa9a1f030
Rakib Hossain <35700333+rakiblegends@users.noreply.github.com>
2025-03-06 17:45:11 +0600

    uploading the half-finished project

---
+++ StaticWebMVC/StaticWeb/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Basic_Static_Web_App.Models;
+
+namespace Basic_Static_Web_App.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+    public IActionResult ContactUs()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
